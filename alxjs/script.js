4c91aacdffc02d05d7fcbaed57f609350c163f46
ale3andro <ale3andro@gmail.com>
2018-05-04 23:57:09 +0300

    Πρώτο commit

---
+++ alxjs/script.js
@@ -0,0 +1,162 @@
+//  level=='1' -> Μόνο δεξί και αριστερό βελάκι
+//  level=='2' -> Μόνο πάνω και κάτω βελάκι
+//  level=='3' -> Όλα τα βελάκια, και τα 4
+
+var supported_levels = ['1' ,'2', '3'];
+
+var level = getJsonFromUrl()['level'];
+if (level==undefined) { level=1; }
+if ($.inArray(level, supported_levels)==-1) { level=1; }
+level=3;
+$('#alxlevel').text(level);
+
+var score = 0;
+var mytimer=30; // δευτερόλεπτα 
+var gameStarted=0; // 0 όταν το παιχνίδι δεν έχει αρχίσει, 1 όταν έχει αρχίσει
+var currentImage=0;
+var newcurrentImage=0;
+var values = [];
+var seed = 0;
+var countdownimage=3;
+
+$('#alxstartbuttoncontainer').html('<input type="button" id="alxstartbutton" class="btn btn-primary" value="Εκκίνηση">');
+$("#alximage").attr("src", "img/X.png");
+$("#alxscore").html(score);
+$("#alxdance").attr("src", "img/pressing_arrows.gif");
+$("#alxtime").html(mytimer);
+
+var clock = $('#alxtime').FlipClock(mytimer, {
+    autostart: false,
+    clockFace: 'Counter',
+    countdown: true,
+  });
+ 
+
+$("#alxstartbutton").click(function() {
+  imageChangeTimer();  
+});
+
+function startGame() {
+  gameStarted=1;
+  clock.start();
+  currentImage=getNextImage(level);
+  $("#alximage").attr("src", "img/" + currentImage + ".png");
+  $('#alxstartbuttoncontainer').html('');
+  setTimeout(timer, 1000);
+}
+
+function imageChangeTimer(){
+  if (countdownimage>0) {
+    $("#alximage").show().attr('src', "img/finger_" + countdownimage + ".png").delay(200).fadeOut(700).delay(100);
+    countdownimage-=1;
+    setTimeout(imageChangeTimer, 1000);
+  }
+  else {
+    $("#alximage").show().attr('src', "img/go-start-game.png").delay(200).effect("pulsate");
+    countdownimage=3;
+    setTimeout(startGame, 1000);
+  }
+}
+
+function timer() {
+  mytimer-=1;
+  clock.decrement();
+  if (mytimer<0) {
+    $('#alxstartbuttoncontainer').html('<input type="button" id="alxsretartbutton" class="btn btn-primary" value="Ξανά!" onclick="location.reload();">');
+    $("#alximage").attr("src", "img/game_over.png");
+    $("#alxdance").attr("src", "img/dance.gif");
+    gameStarted=0;
+    $('#alx_hiddenscoreform').html('<form id="alx_invisible_form" action="score.php" method="post"><input name="score" type="hidden" value="' + score + '"></form>');
+    $('#alx_invisible_form').submit();
+  }
+  else {
+    setTimeout(timer, 1000);
+  }
+}
+
+$("body").on("keydown", function (e) {
+  if (gameStarted==1) {
+    if ((currentImage+37)==e.which) {
+      score+=1;
+      do {
+        newcurrentImage=getNextImage(level);
+      }
+      while (newcurrentImage==currentImage);
+      currentImage=newcurrentImage;
+      $("#alximage").attr("src", "img/" + currentImage + ".png");
+      $("#alxscore").html(score);
+    }
+    else {
+      if (score>0) {
+        score-=1;
+        $("#alxscore").html(score);
+      }
+    }
+  }
+
+  switch(e.which) {
+        case 37: // left
+        break;
+
+        case 38: // up
+        break;
+
+        case 39: // right
+        break;
+
+        case 40: // down
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
+function getJsonFromUrl() {
+  var query = location.search.substr(1);
+  var result = {};
+  query.split("&").forEach(function(part) {
+    var item = part.split("=");
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+  return result;
+}
+
+function getNextImage(theLevel) {
+  if (theLevel=='1') {
+    // Πάνω και κάτω βελάκι
+    // Πρέπει να επιστρέψω είτε 1 (για το πάνω βελάκι) είτε 3 (για το κάτω βελάκι)
+    values=[1,3];
+    seed = Math.round(Math.random());
+    return values[seed];
+  }
+  else if (theLevel=='2') {
+    // Πάνω και κάτω βελάκι
+    // Πρέπει να επιστρέψω είτε 0 (για το αριστερό βελάκι) είτε 2 (για το δεξί βελάκι)
+    values=[0,2];
+    seed = Math.round(Math.random());
+    return values[seed];
+  }
+  else if (level=='3') {
+    return Math.floor(Math.random() * 4);
+  }
+
+}
+
+function sendScore() {
+  var request = $.ajax({
+    url: "score.php",
+    type: "POST",
+    data: { score: 99,
+            },
+    dataType: "html"
+  });
+  
+  request.done(function(msg) {
+    alert(msg);
+  });
+  
+  request.fail(function(jqXHR, textStatus) {
+    alert( "Request failed: " + textStatus );
+  });
+}
---
+++ score.php
@@ -0,0 +1,231 @@
+<?php
+    function get_form($score) {
+        return '<form class="form-horizontal" method="post" action="score.php">
+        <fieldset>
+        
+        <!-- Form Name -->
+        <legend>Καταχώρηση σκορ</legend>
+        
+        <!-- Text input-->
+        <input hidden id="frm_score" name="frm_score" value="' . $score . '" class="form-control input-md" required="" type="text">
+                
+        <!-- Text input-->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="frm_name">Όνομα:</label>  
+          <div class="col-md-4">
+          <input id="frm_name" name="frm_name" placeholder="Εδώ γράψε το όνομα σου" class="form-control input-md" required="" type="text" oninvalid="this.setCustomValidity(\'Το όνομα είναι υποχρεωτικό\')" oninput="setCustomValidity(\'\')">
+            
+          </div>
+        </div>
+        
+        <!-- Select Basic -->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="frm_class">Τάξη</label>
+          <div class="col-md-4">
+            <select id="frm_class" name="frm_class" class="form-control">
+              <option value="0">Α τάξη</option>
+              <option value="1">Β τάξη</option>
+              <option value="2">Γ τάξη</option>
+              <option value="3">Δ τάξη</option>
+              <option value="4">Ε τάξη</option>
+              <option value="5">ΣΤ τάξη</option>
+            </select>
+          </div>
+        </div>
+        
+        <!-- Select Basic -->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="frm_school">Σχολείο:</label>
+          <div class="col-md-4">
+            <select id="frm_school" name="frm_school" class="form-control">
+              <option value="0">1ο Δημοτικό Γιαννιτσών</option>
+              <option value="1">6ο Δημοτικό Γιαννιτσών</option>
+              <option value="2">Άλλο</option>
+            </select>
+          </div>
+        </div>
+        
+        <!-- Button -->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="frm_submit"></label>
+          <div class="col-md-4">
+            <button id="frm_submit" name="frm_submit" class="btn btn-primary">Αποθήκευση</button>
+          </div>
+        </div>
+        
+        </fieldset>
+        </form>
+        ';
+    }
+
+    function get_table_line($score, $date, $name, $class, $school) {
+      return '<div class="row">
+                  <div class="col-sm-1">' . $score . '</div>
+                  <div class="col-sm-1 col-lg-2">' . $date. '</div>
+                  <div class="col-sm-1 col-lg-2">' . $name . '</div>
+                  <div class="col-sm-1 col-lg-2">' . $class . '</div>
+                  <div class="col-sm-1 col-lg-4">' . $school . '</div>
+              </div>';
+    }
+
+    $db_filename = "high_scores/high_scores.db";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Τα βελάκια του πληκτρολογίου</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/blog-post.css" rel="stylesheet">
+    <!-- timer flipclock css -->
+    <link rel="stylesheet" href="css/flipclock.css">
+
+  </head>
+
+  <body>
+
+
+
+    <a href="https://github.com/ale3andro/keyboard_arrows_game"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/567c3a48d796e2fc06ea80409cc9dd82bf714434/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png"></a>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+
+        <!-- Post Content Column -->
+        <div class="col-lg-8">
+
+          <!-- Title -->
+          <h1 class="mt-4">Βελάκια Πληκτρολογίου</h1>
+
+          <?php 
+                if (isset($_POST['score'])) {
+                  $db = new SQLite3($db_filename);
+                  $results = $db->query('SELECT (SELECT count() FROM scores where score>' . $_POST['score'] . ') as count, * from scores');
+                  if ($results->fetchArray()[0]>=10) {
+                    echo '<div class="container"><div class="row">';
+                    echo '<div class="col-sm-5"><div class="card my-4"><h5 class="card-header">Αποτέλεσμα</h5><div class="card-body"><div class="row"><div class="col-lg-12"><img src="img/sad_face.png" />
+                            </div></div></div></div></div>';
+                    echo '<div class="col-sm-5"><div class="card my-4"><h5 class="card-header">Το Σκορ σου</h5><div class="card-body"><div class="row"><div class="col-lg-12">
+                            <div id="alxscore">' . $_POST['score'] . '</div></div></div></div></div></div>';
+                    echo '<h2><span class="badge badge-warning">Δυστυχώς, με σκορ ' . $_POST['score'] . ', δεν μπαίνεις στο Hall of Fame...</span></h2>';
+                    echo '<div class="col-sm-5"><div class="card my-4"><h5 class="card-header">Hall of Fame</h5><div class="card-body"><div class="row"><div class="col-lg-12">
+                            <a href="score.php"><img src="img/hall-of-fame.png" /></a></div></div></div></div></div>';
+                    echo '<div class="col-sm-5"><div class="card my-4"><h5 class="card-header">Παίξε πάλι!</h5><div class="card-body"><div class="row"><div class="col-lg-12">
+                            <a href="index.html"><img src="img/play_again.png" /></a></div></div></div></div></div>';
+                    echo "</div></div>";
+                  }
+                  else { 
+                    echo '<div class="container"><div class="row">';
+                    echo '<div class="col-sm-5"><div class="card my-4"><h5 class="card-header">Αποτέλεσμα</h5><div class="card-body"><div class="row"><div class="col-lg-12"><img src="img/happy_face.png" />
+                            </div></div></div></div></div>';
+                    echo '<div class="col-sm-5"><div class="card my-4"><h5 class="card-header">Το Σκορ σου</h5><div class="card-body"><div class="row"><div class="col-lg-12">
+                            <div id="alxscore">' . $_POST['score'] . '</div></div></div></div></div></div>';  
+                    echo "</div></div>";              
+                    echo get_form($_POST['score']);
+                  }
+                } elseif (isset($_POST['frm_name'])) {
+                    switch ($_POST['frm_class']) {
+                      case 0:
+                        $s_class = "Α τάξη";
+                        break;
+                      case 1:
+                        $s_class = "Β τάξη";
+                        break;
+                      case 2:
+                        $s_class = "Γ τάξη";
+                        break;
+                      case 3:
+                        $s_class = "Δ τάξη";
+                        break;
+                      case 4:
+                        $s_class = "Ε τάξη";
+                        break;
+                      case 5:
+                        $s_class = "ΣΤ τάξη";
+                        break;
+                    }
+                    switch ($_POST['frm_school']) {
+                      case 0:
+                        $s_school = "1ο Δημοτικό Γιαννιτσών";
+                        break;
+                      case 1:
+                        $s_school = "6ο Δημοτικό Γιαννιτσών";
+                        break;
+                      case 2:
+                        $s_school = "Άλλο";
+                        break;
+                    }
+                    $s_date = date("Y-m-d");
+                    shell_exec('sudo chgrp -R www-data high_scores && sudo chmod -R 775 high_scores');
+                    $db = new SQLite3($db_filename);
+                    $db->exec('BEGIN');
+                    $command = 'insert into scores values (' . $_POST['frm_score'] . ', "' . $s_date . '", "' . $_POST['frm_name'] . '", "' . $s_class . '", "' . $s_school . '")';
+                    if (!$db->exec($command))
+	                    die("unable to save");
+                    $db->exec('COMMIT');
+                    echo '<div class="container"><div class="row">';
+                    echo '<h2><span class="badge badge-success">Το σκορ σου αποθηκεύτηκε με επιτυχία!</span></h2>';
+                    echo '<div class="col-sm-5"><div class="card my-4"><h5 class="card-header">Hall of Fame</h5><div class="card-body"><div class="row"><div class="col-lg-12">
+                            <a href="score.php"><img src="img/hall-of-fame.png" /></a></div></div></div></div></div>';
+                    echo '<div class="col-sm-5"><div class="card my-4"><h5 class="card-header">Παίξε πάλι!</h5><div class="card-body"><div class="row"><div class="col-lg-12">
+                            <a href="index.html"><img src="img/play_again.png" /></a></div></div></div></div></div>';
+                    echo "</div></div>";
+                } else {
+                    echo '<!-- Author -->
+                            <p class="lead">Hall of Fame - Οι 10 καλύτεροι χρόνοι</p>
+                            <hr>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-sm-1">Σκορ</div>
+                                    <div class="col-sm-1 col-lg-2">Ημερομηνία</div>
+                                    <div class="col-sm-1 col-lg-2">Όνομα</div>
+                                    <div class="col-sm-1 col-lg-2">Τμήμα</div>
+                                    <div class="col-sm-1 col-lg-2">Σχολείο</div>
+                                </div>';
+                    $db = new SQLite3($db_filename);
+                    $results = $db->query('SELECT * FROM scores order by score DESC, date ASC limit 10');
+                    while ($row = $results->fetchArray()) {
+                        echo get_table_line($row[0], $row[1], $row[2], $row[3], $row[4]);
+                    }
+                    echo '</div>';
+                    echo '<div class="col-sm-5"><div class="card my-4"><h5 class="card-header">Παίξε πάλι!</h5><div class="card-body"><div class="row"><div class="col-lg-12">
+                            <a href="index.html"><img src="img/play_again.png" /></a></div></div></div></div></div>';
+                }
+
+        ?>
+          
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyleft - <a href="http://ale3andro.gr" target="_blank">ale3andro.gr</a></p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/jquery/jquery-ui.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="alxjs/flipclock.min.js"></script>
+
+  </body>
+
+</html>
