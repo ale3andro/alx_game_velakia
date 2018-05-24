09fe7e635321290f18f6b6270ef805a726c1f71c
ale3andro <ale3andro@gmail.com>
2018-05-24 22:37:49 +0300

    Διόρθωση bug λάθους εμφάνισης του high score

--- score.php
+++ score.php
@@ -84,7 +84,7 @@
     <title>Τα βελάκια του πληκτρολογίου</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../bower_components/bootstrap4/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="css/blog-post.css" rel="stylesheet">
